79bbb9394148d1c804916d6d7dfd4b5fe0855259
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-10-12 02:16:46 -0400

    classnames for dynamic classes, outline on drop

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 import { useDrop } from "react-dnd";
+import cx from "classnames";
 import Item from "./Item";
 
 const Canvas = () => {
-    const [canvasItems, setCanvasItems] = useState<React.ReactNode>([]);
+  const [canvasItems, setCanvasItems] = useState<React.ReactNode>([]);
 
-    const [{ isOver }, drop] = useDrop({
-        accept: "toolbarItem",
-        drop: (item) => {
-            setCanvasItems((canvasItems: any) => [...canvasItems, item]);
-        },
-        collect: (monitor) => ({
-            isOver: monitor.isOver(),
-        })
-    });
+  const [{ isOver }, drop] = useDrop({
+    accept: "toolbarItem",
+    drop: (item) => {
+      setCanvasItems((canvasItems: any) => [...canvasItems, item]);
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+  });
 
   return (
-    <div ref={drop} className="h-[calc(100vh-6rem)] w-[calc(100vw-12rem)] absolute top-24 left-48 bg-[#242020] flex flex-row flex-wrap justify-evenly content-start">
-        {(canvasItems as any[]).map((item: any) => (
-            <Item name={item.name} />
-        ))}
+    <div
+      ref={drop}
+      className={cx(
+        "h-[calc(100vh-6rem)] w-[calc(100vw-12rem)] absolute top-24 left-48 bg-[#242020] flex flex-row flex-wrap justify-evenly content-start",
+        isOver && "outline-dashed outline-4 outline-green outline-offset-[-4px]"
+      )}
+    >
+      {(canvasItems as any[]).map((item: any) => (
+        <Item name={item.name} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Canvas;
