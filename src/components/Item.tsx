dd553abc619d902b9bb3e605a4bf116c8ba9c1db
deko95 <ks1379@scarletmail.rutgers.edu>
2022-10-16 15:13:27 -0400

    Fix cancel/confirm behavior inside Item
    Add keyboard events on Esc and Enter to cancel and comfirm changes

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   name: string;
@@ -6,30 +6,59 @@ type Props = {
 
 const Item = ({ name }: Props) => {
   const [content, setContent] = useState("");
+  const [savedContent, setSavedContent] = useState("");
   const [isConfirmed, setIsConfirmed] = useState(false);
-  const [isCancelled, setIsCancelled] = useState(false);
 
-  let confirmedItem = (
-    <div className="h-24 w-40 m-10 bg-jet text-xl text-cream rounded-xl border border-cream border-opacity-25 shadow-[8px_8px_rgba(0,0,0,0.4)] flex justify-center items-center">
+  const handleConfirm = () => {
+    setSavedContent(content);
+    setIsConfirmed(true);
+  };
+
+  const handleCancel = () => {
+    setContent(savedContent);
+    setIsConfirmed(true);
+  };
+
+  const handleKeyDown = (e: any) => {
+    console.log(e.key);
+    e.key === "Enter" && handleConfirm();
+    e.key === "Escape" && handleCancel();
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+
+  }, [handleKeyDown])
+
+  const confirmedItem = content !== "" && (
+    <div
+      className="h-24 w-40 m-10 bg-jet text-xl text-cream rounded-xl border border-cream border-opacity-25 shadow-[8px_8px_rgba(0,0,0,0.4)] flex justify-center items-center"
+      onClick={() => setIsConfirmed(false)}
+    >
       {content}
     </div>
   );
 
-  let unconfirmedItem = (
-    <div className="h-[80%] w-[80%] absolute m-10 bg-jet text-xl text-cream rounded-xl border border-cream border-opacity-25 shadow-[24px_24px_rgba(0,0,0,0.4)] flex justify-center items-center">
+  const editingModal = (
+    <div className="h-[calc(100%-80px)] w-[calc(100%-80px)] absolute m-10 bg-jet text-xl text-cream rounded-xl border border-cream border-opacity-25 shadow-[24px_24px_rgba(0,0,0,0.4)] flex justify-center items-center" onKeyDown={handleKeyDown}>
       <input
         className="h-12 w-40 text-xl text-cream bg-jet rounded-xl border"
         placeholder="Enter text"
         value={content}
         onChange={(e) => setContent(e.target.value)}
+        autoFocus
       ></input>
+
       <button
         className="h-12 w-40 text-xl text-cream bg-jet rounded-xl border"
         {...(content === "" && { disabled: true })}
-        onClick={() => {setIsConfirmed(true)}}
+        onClick={handleConfirm}
       >
         Confirm
       </button>
+
       <button
         className="h-12 w-40 text-xl text-cream bg-jet rounded-xl border"
         onClick={() => setContent("")}
@@ -39,15 +68,14 @@ const Item = ({ name }: Props) => {
 
       <button
         className="h-12 w-40 text-xl text-cream bg-jet rounded-xl border"
-        onClick={() => setIsCancelled(true)}
+        onClick={handleCancel}
       >
         Cancel
       </button>
     </div>
   );
-  return (
-    <>{isCancelled ? null : isConfirmed ? confirmedItem : unconfirmedItem}</>
-  );
+
+  return <>{isConfirmed ? confirmedItem : editingModal}</>;
 };
 
 export default Item;
