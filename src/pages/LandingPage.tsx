4ecc72f11b1d1bb229af5fd61eed2eb6864433dc
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-10-17 16:09:07 -0400

    Tailwind desktop-first patch

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -11,8 +11,9 @@ const LandingPage = ({ setIsLoggedIn, setLoginRequest, setRegisterRequest }: Pro
     <div className="bg-dark-jet h-screen w-full">
       <button className="button-blue absolute right-24 top-6" onClick={() => setLoginRequest(true)}>Login</button>
       <button className="absolute text-cream underline underline-offset-4 top-8 right-5" onClick={() => setRegisterRequest(true)}>Sign Up</button>
-      <div className="bg-[#171313] flex flex-col items-center h-screen w-4/12">
-        <h1 className="text-cream font-bold text-4xl tracking-[0.13em] leading-normal mt-32">
+      <button className="xs-min:hidden bg-cream">hide/show</button>
+      <div className="bg-[#171313] flex flex-col items-center h-screen w-4/12 xs:hidden">
+        <h1 className="text-cream font-bold text-4xl sm:text-base md:text-2xl lg:text-3xl tracking-[0.13em] leading-normal mt-32">
           <span className="text-blue">d</span>igital&nbsp;
           <span className="text-green">g</span>arden<br></br>
           <span className="text-pink">w</span>orkspace<br></br>
@@ -27,7 +28,7 @@ const LandingPage = ({ setIsLoggedIn, setLoginRequest, setRegisterRequest }: Pro
       <a href="https://github.com/TheRoyalTea/Digital-Garden-Workspace-Collaboration-Tool-Application-For-Productivity">
         <img
           src={GitHubLogo}
-          className="absolute h-12 w-12 bottom-6 left-6"
+          className="absolute h-12 w-12 bottom-6 left-6 md:h-8 md:w-8 xs-min:hidden"
         ></img>
       </a>
     </div>
