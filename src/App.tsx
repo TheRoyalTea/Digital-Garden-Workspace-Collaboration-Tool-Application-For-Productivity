6bab7afcc8e1d5b83ceaa68a1409638d5ebde6bc
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-10-19 13:25:39 -0400

    Basic auth with UI provided by Amplify Vite config fix to run Amplify dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import Register from "./components/Register";
 import Board from "./pages/Board";
 import LandingPage from "./pages/LandingPage";
 
-const App = () => {
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+//@ts-ignore
+const App = ({ signOut, user}) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loginRequest, setLoginRequest] = useState(false);
   const [registerRequest, setRegisterRequest] = useState(false);
   
   return (
     <>
+          <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
       {isLoggedIn ? <Board /> : <LandingPage setIsLoggedIn={setIsLoggedIn} setLoginRequest={setLoginRequest} setRegisterRequest={setRegisterRequest}/>}
       {loginRequest && <Login setIsLoggedIn={setIsLoggedIn} setLoginRequest={setLoginRequest} setRegisterRequest={setRegisterRequest}/>}
       {registerRequest && <Register setLoginRequest={setLoginRequest} setRegisterRequest={setRegisterRequest}/>}
@@ -18,4 +23,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Amplify } from 'aws-amplify';
+//@ts-ignore
+import config from './aws-exports.js'
+Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    {/*@ts-ignore*/}
     <App />
   </React.StrictMode>
 )
