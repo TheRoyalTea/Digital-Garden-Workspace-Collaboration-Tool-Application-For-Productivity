918c7545f1c0ab3dc57f6475c0199b725952d38a
TheRoyalTea <np768@scarletmail.rutgers.edu>
2022-10-20 12:22:05 -0400

    Data Model Backend :D

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "DigitalGardenWorkspaceCollaborationToolApplicationForProductivity": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
