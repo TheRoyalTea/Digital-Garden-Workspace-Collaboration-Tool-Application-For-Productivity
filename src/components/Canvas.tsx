efd0aeb4069db816fe0beae543de29f7dfb11d1c
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-11-17 23:35:01 -0500

    add layout persistence

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDrop } from "react-dnd";
+import { API } from "aws-amplify";
 import cx from "classnames";
 import Item from "./Item";
 import ItemModal from "./ItemModal";
-import { Responsive, WidthProvider, Layout, Layouts } from "react-grid-layout";
+import { getCanvas, updateCanvas } from "../graphql";
+import { Responsive, WidthProvider, Layouts } from "react-grid-layout";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 import "../assets/css/react-grid-layout.css";
 import "../assets/css/react-resizable.css";
@@ -24,19 +26,74 @@ const Canvas = ({
   requestedModal,
   setRequestedModal,
 }: Props) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [layout, setLayout] = useState<any[]>([]);
+  const [firstRender, setFirstRender] = useState(true);
+  const [layouts, setLayouts] = useState<Layouts>();
 
   const [{ isOver }, drop] = useDrop({
     accept: "toolbarItem",
     drop: (item: any) => {
       setRequestedModal(item.type);
-      console.log(item);
     },
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
   });
+
+  const createLayout = () => {
+    const newLayouts: Layouts = {};
+    const sizes = ["lg", "md", "sm", "xs", "xxs"];
+    let y = 0;
+    canvasItems.forEach((item: any) => {
+      sizes.forEach((size) => {
+        if (!newLayouts[size]) {
+          newLayouts[size] = [];
+        }
+        newLayouts[size].push({
+          i: item.id,
+          x: 0,
+          y: y,
+          // TODO: way to calculate width and height based on content
+          w: 2,
+          h: 2,
+        });
+      });
+      y += 2;
+    });
+    setLayouts(newLayouts);
+  };
+
+  const fetchSavedLayouts = async () => {
+    const canvas: any = await API.graphql({
+      query: getCanvas,
+      variables: {
+        id: activeCanvas.id,
+      },
+    });
+    if (canvas.data.getCanvas.layouts) {
+      setLayouts(JSON.parse(canvas.data.getCanvas.layouts));
+    }
+  };
+
+  const persistLayouts = async (_layouts: Layouts) => {
+    try {
+      await API.graphql({
+        query: updateCanvas,
+        variables: {
+          input: {
+            id: activeCanvas.id,
+            layouts: JSON.stringify(_layouts),
+          },
+        },
+      });
+      fetchSavedLayouts();
+    } catch (err) {
+      console.error("error updating canvas", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchSavedLayouts();
+  }, []);
 
   return (
     <div
@@ -48,26 +105,22 @@ const Canvas = ({
       )}
     >
       <ResponsiveGridLayout
+        layouts={layouts}
         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
         rowHeight={50}
         margin={[15, 15]}
-        onLayoutChange={(layout, layouts) => {
-          console.log("current layout: ", layout);
-          console.log("layouts", layouts);
+        onLayoutChange={(_layout, _layouts) => {
+          !firstRender && persistLayouts(_layouts);
+          setFirstRender(false);
+          // FIXME?: see if there is a way to work around having to do this on every layout change
+          console.log("client: ", _layouts);
+          console.log("server: ", JSON.parse(activeCanvas.layouts));
         }}
         // TODO: change resize handles dynamically based on item's position
         resizeHandles={["se"]}
       >
         {(canvasItems as any[]).map((item: any, idx: number) => (
-          <div
-            key={"i" + idx}
-            data-grid={{
-              x: idx,
-              y: Math.floor(idx / 5) * Math.ceil(Math.random() * 4),
-              w: Math.ceil(Math.random() * 4),
-              h: Math.ceil(Math.random() * 4),
-            }}
-          >
+          <div key={item.id}>
             <Item data={item} setRequestedModal={setRequestedModal} />
           </div>
         ))}
